eaa816460892e6599a1a48d9e450fbc6f0c7b380
David Lie <vicecersaa@gmail.com>
2022-11-18 17:59:55 +0700

    commit change

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,3 +1,59 @@
+import {Header} from '../components/Header'
+import {Navbar} from '../components/Navbar'
+import {Footer} from '../components/Footer'
+import { Title } from '../components/Title'
+import { ShopSection } from '../components/ShopSection'
+import WellingtonFull from '../img/WellingtonFull.png'
+import SwanstonFull from '../img/SwanstonFull.png'
+import BrightonFull from '../img/BrightonFull.png'
+import GlenIrisFull from '../img/GlenIrisFull.png'
+import Sofa from '../img/Tampak Depan.png'
+import MorningtonBonnel from '../img/MorningtonBonnelFull.png'
+import MorningtonPocket from '../img/MorningtonPocketFull.png'
+import Morrison from '../img/MorrisonFull.png'
+
 export const Product = () => {
-  return <h1>THIS IS THE PRODUCT PAGE</h1>
+  return (
+    <div className='product-page'>
+    <Header />
+    <Navbar />
+    <div className='product-sect'>
+      <div className='shop-title'>
+        <h3>Our Featured</h3>
+        <p>Here you can check out our latest product.</p>
+      </div>
+      
+        <ShopSection 
+          gambar1={WellingtonFull}
+          nameOne="Forland Wellington"
+          gambar2={SwanstonFull}
+          nameTwo="Forland Swanston"
+          gambar3={BrightonFull}
+          nameThree="Forland Brighton"
+          gambar4={GlenIrisFull}
+          nameFour="Forland Glen Iris"
+        />
+
+        <ShopSection 
+          tag="Our Top Products"
+          gambar1={Morrison}
+          nameOne="Forland Morrison"
+          gambar2={MorningtonPocket}
+          nameTwo="Forland Mornington Pocket"
+          gambar3={MorningtonBonnel}
+          nameThree="Forland Mornington Bonnel"
+          gambar4={Sofa}
+          nameFour="Forland Sofa"
+        />
+      <div className='pagination'>
+        <a>Previous</a>
+        <a>1</a>
+        <a>2</a>
+        <a>3</a>
+        <a>Next</a>
+      </div>
+    </div>
+    <Footer />
+    </div>
+  )
 }
